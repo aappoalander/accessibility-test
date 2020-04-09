18e9fba0fcc0906381b74c12404817c87494a0e6
Aappo Ålander <aappo.alander@gofore.com>
2020-04-09 10:20:11 +0300

    Updates

---
+++ precache-manifest.be952b9080e83b64955dba6cd1390f36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f714e55c86194d999c41ee4d717e94",
+    "url": "/accessibility-test/index.html"
+  },
+  {
+    "revision": "6bfd3f112efa5109cf68",
+    "url": "/accessibility-test/static/css/2.a1eb177a.chunk.css"
+  },
+  {
+    "revision": "58a5175023e7aa978459",
+    "url": "/accessibility-test/static/css/main.75b97b44.chunk.css"
+  },
+  {
+    "revision": "6bfd3f112efa5109cf68",
+    "url": "/accessibility-test/static/js/2.b8591575.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/accessibility-test/static/js/2.b8591575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a5175023e7aa978459",
+    "url": "/accessibility-test/static/js/main.476fb4d0.chunk.js"
+  },
+  {
+    "revision": "70a3277b8b5650bb0854",
+    "url": "/accessibility-test/static/js/runtime-main.3f3b3862.js"
+  }
+]);
